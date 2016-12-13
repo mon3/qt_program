ec8511df62c181facd1b0cab4fbcbe4fa195602f
mon3 <seniut.monika@gmail.com>
2016-12-14 00:32:15 +0100

    not working

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+#include <qwt.h>
+#include <qwt/qwt_plot.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    // Tworzenie okna programu
+    QWidget okno;
+    okno.setWindowTitle ("Dopasowanie MNK"); // ustawia tytuł okna
+    okno.setFixedSize (640, 480); // rozmiar okna 800x800
+    // (...) rysowanie na oknie
+
+    // Tworzenie wykresu na konkretnym oknie
+    QwtPlot wykres (&okno);
+    // Ustawienie tytułu wykresu
+    wykres.setTitle ("Dopasowanie MNK");
+    // Ustawienia tytułów osi: yLeft, yRight, xBottom, xTop
+    wykres.setAxisTitle (QwtPlot::xBottom, "Nr kanału");
+    wykres.setAxisTitle (QwtPlot::yLeft, "Napięcie / V");
+    // Ustawienie rozmiarów wykresu
+    wykres.setFixedSize (775, 775);
+    // Ustawienie koloru tła
+    wykres.setCanvasBackground (QColor (0xff,0xfa, 0x6b));
+    // Ustawienie zakresu osi
+//    wykres.setAxisScale (QwtPlot::xBottom, min_x, max_x);
+//    wykres.setAxisScale (QwtPlot::yLeft, min_y, max_y);
+
+
+
+    okno.show();
+
+    return a.exec();
+}
---
+++ prog6.pro
@@ -0,0 +1,28 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-12-13T23:34:50
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = prog6
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+CONFIG += Qwt
+
+INCLUDEPATH += /usr/local/qwt-5.2.4-svn/include
+LIBS += -L/usr/local/qwt-5.2.4-svn/lib -lqwt
+INCLUDEPATH += /home/monika03/Downloads/libpng-1.6.26/.libs/
+LIBS += -L/home/monika03/Downloads/libpng-1.6.26/.libs/libpng16.so.16
+
